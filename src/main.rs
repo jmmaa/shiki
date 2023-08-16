49cfee021c9505baaf3869fc756f75f3c17d32a5
arkhon7 <reviuy9@gmail.com>
2023-08-16 19:18:32 +0800

    updated tests

--- src/main.rs
+++
@@ -1,5 +0,0 @@
-use shiki::parser::number::float;
-
-fn main() {
-    println!("{:?}", float(b"-0.1", 0))
-}
--- tests/test_shiki.rs
+++ tests/test_shiki.rs
@@ -4,5 +4,7 @@ use shiki::parser::number::number;
 fn test_crate() {
     let value = number(b"-0.1", 0).unwrap().0;
 
+    println!("{value:?}");
+
     assert_eq!(value, b"-0.1")
 }
