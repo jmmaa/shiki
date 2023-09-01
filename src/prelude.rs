e0843a4dcab4fb30e48d7ee519e13253dcebbfd9
arkhon7 <reviuy9@gmail.com>
2023-09-01 19:26:33 +0800

    redefined types

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,4 +2,10 @@ pub use crate::error::Error;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
+pub type Parser<T> = fn(Source, Position) -> Result<(T, Source, Position)>;
+
+pub type Source<'src> = &'src [u8];
+
+pub type Position = usize;
+
 pub use std::format as f;
