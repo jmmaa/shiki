24fd2a3bdf26d7c73f395bc7032fe48033bac3ef
arkhon7 <reviuy9@gmail.com>
2023-09-01 19:26:13 +0800

    added docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
+//!
+//! This project is inspired from the Computerphile Video of Graham Hutton
+//! Link: https://youtu.be/dDtZLm7HIJs?si=khUftQbWYf-87NHZ
+//!
+//!
+//! Basically, this is a markup language made from multiple little parsers. To improve from the idea,
+//! I used slices instead of strings, this is to minimize memory allocation overhead.
+//!
+//! So instead of this signature based from the video:
+//! ```rust
+//! type Parser<T> = fn(String) -> (T, String);
+//! ```
+//!
+//! The parsers will follow this one:
+//!
+//! ```rust
+//! type Parser<T> = fn(Source, Position) -> (T, Source, Position);
+//!     
+//! type Source<'src> = &'src [u8]; // The reference on source bytes
+//!
+//! type Position = usize; // The current reading position
+//! ```
+//! Some parsers will have different signature depending on its needs, but it will still implement
+//! a superset of type `Parser<T>`.
 pub mod error;
 pub mod parser;
 pub mod prelude;
 pub mod utils;
-
-// ADD ERROR HANDLING LIBRARY, thiserror, anyhow
-// THIHK ABOUT THE PARSING MECHANISM
