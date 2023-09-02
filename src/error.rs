41606f46dc05b12f5cfb4ba13643653749de44b1
arkhon7 <reviuy9@gmail.com>
2023-09-02 16:46:20 +0800

    draft

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,16 @@
+// use thiserror::Error;
+
+// use crate::prelude::{Position, Source};
+
+// #[derive(Error, Debug)]
+// pub enum Error<'src, T> {
+//     #[error("{0}")]
+//     Generic(String),
+
+//     #[error("{0}")]
+//     Parser((T, Source<'src>, Position)),
+// }
+
 use thiserror::Error;
 
 #[derive(Error, Debug)]
